acbe68bc7ffbe34ee923db6d8c991a98435846f6
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2022-11-14 21:12:22 -0300

    Overlay fixed! :)

--- HelloOpenCV-Python/Overlay.py
+++ HelloOpenCV-Python/Overlay.py
@@ -1,37 +1,53 @@
 import cv2
 import numpy as np
 
-def logoOverlay(image,logo,alpha=1.0,x=0, y=0, scale=1.0):
-    (h, w) = image.shape[:2]
-    image = np.dstack([image, np.ones((h, w), dtype="uint8") * 255])
-    
-    overlay = cv2.resize(logo, None,fx=scale,fy=scale)
-    (wH, wW) = overlay.shape[:2]
-    output = image.copy()
-    # blend the two images together using transparent overlays
-    try:
-        if x<0 : x = w+x
-        if y<0 : y = h+y
-        if x+wW > w: wW = w-x  
-        if y+wH > h: wH = h-y
-        print(x,y,wW,wH)
-        overlay=cv2.addWeighted(output[y:y+wH, x:x+wW],alpha,overlay[:wH,:wW],1.0,0)
-        output[y:y+wH, x:x+wW ] = overlay
-    except Exception as e:
-        print("Error: Logo position is overshooting image!")
-        print(e)
-    
-    output= output[:,:,:3]
-    return output
+def overlay(background, foreground, x_offset=None, y_offset=None):
+    bg_h, bg_w, bg_channels = background.shape
+    fg_h, fg_w, fg_channels = foreground.shape
+
+    assert bg_channels == 3, f'background image should have exactly 3 channels (RGB). found:{bg_channels}'
+    assert fg_channels == 4, f'foreground image should have exactly 4 channels (RGBA). found:{fg_channels}'
+
+    # center by default
+    if x_offset is None: x_offset = (bg_w - fg_w) // 2
+    if y_offset is None: y_offset = (bg_h - fg_h) // 2
+
+    w = min(fg_w, bg_w, fg_w + x_offset, bg_w - x_offset)
+    h = min(fg_h, bg_h, fg_h + y_offset, bg_h - y_offset)
+
+    if w < 1 or h < 1: return
+
+    # clip foreground and background images to the overlapping regions
+    bg_x = max(0, x_offset)
+    bg_y = max(0, y_offset)
+    fg_x = max(0, x_offset * -1)
+    fg_y = max(0, y_offset * -1)
+    foreground = foreground[fg_y:fg_y + h, fg_x:fg_x + w]
+    background_subsection = background[bg_y:bg_y + h, bg_x:bg_x + w]
+
+    # separate alpha and color channels from the foreground image
+    foreground_colors = foreground[:, :, :3]
+    alpha_channel = foreground[:, :, 3] / 255  # 0-255 => 0.0-1.0
+
+    # construct an alpha_mask that matches the image shape
+    alpha_mask = np.dstack((alpha_channel, alpha_channel, alpha_channel))
+
+    # combine the background with the overlay image weighted by alpha
+    composite = background_subsection * (1 - alpha_mask) + foreground_colors * alpha_mask
+
+    # overwrite the section of the background image that has been updated
+    background[bg_y:bg_y + h, bg_x:bg_x + w] = composite
+    out = background.copy()
+    return out
 
 background = cv2.imread('baboon.png')
-overlay = cv2.imread('flaming_meteor.png', cv2.IMREAD_UNCHANGED)
+foreground = cv2.imread('eyeglasses.png', cv2.IMREAD_UNCHANGED)
 
-print(overlay.shape) # must be (x,y,4)
+print(foreground.shape) # must be (x,y,4)
 print(background.shape) # must be (x,y,3)
 
 # downscale logo by half and position on bottom right reference
-out = logoOverlay(background,overlay,scale=2.0,y=-100,x=-100) 
+out = overlay(background,foreground,100, 40) 
 
-cv2.imshow("test",out)
+cv2.imshow('yeah',out)
 cv2.waitKey(0)
